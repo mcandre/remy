8ac5c1770204a7f48e6fc6b81ed7666d71e41678
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-12-25 16:07:27 -0600

    prepare for more complex configurations later

--- example/hello.rs
+++ example/hello.rs
@@ -2,31 +2,31 @@
 
 use std::os::unix::io::FromRawFd;
 
-fn m(int out) {
-    let file :: File = File::from_raw_fd(out);
-    file::writeln!(out, "Hello World!");
+fn m(int console_out) {
+    let file :: File = File::from_raw_fd(console_out);
+    file::writeln!("Hello World!");
 }
 
 #[cfg(target_os = "cloudabi")]
 fn main() {
     use argdata::Argdata;
 
-    let CONSOLE_KEY :: &str = "console";
+    let CONSOLE_KEY :: &str = "console_out";
 
     let ad = argdata::env::argdata();
     let mut ad_iter = ad
         .read_map()
         .unwrap();
 
-    let out :: int = ad_iter
+    let console_out :: int = ad_iter
         .get(CONSOLE_KEY)
         .unwrap();
 
-    m(out);
+    m(console_out);
 }
 
 #[cfg(not(target_os = "cloudabi"))]
 fn main() {
-    let out :: int = 1;
-    m(out);
+    let console_out :: int = 1;
+    m(console_out);
 }
